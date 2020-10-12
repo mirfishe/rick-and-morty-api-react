27942a44c269593319d3fb0792c13a004ed0f464
Michael Fisher <mirfishe@gmail.com>
2020-10-12 11:57:32 -0400

    Updates

---
+++ precache-manifest.7ce241f15a6f3503425b290d6cba1e04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b592df3c1ae065a885d43d0473e43b0",
+    "url": "/rick-and-morty-api-react/index.html"
+  },
+  {
+    "revision": "988e4c6f02ca7a930e06",
+    "url": "/rick-and-morty-api-react/static/css/2.e712defa.chunk.css"
+  },
+  {
+    "revision": "d8e557b43a9c1d68b142",
+    "url": "/rick-and-morty-api-react/static/css/main.2e731041.chunk.css"
+  },
+  {
+    "revision": "988e4c6f02ca7a930e06",
+    "url": "/rick-and-morty-api-react/static/js/2.80ddd540.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/rick-and-morty-api-react/static/js/2.80ddd540.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8e557b43a9c1d68b142",
+    "url": "/rick-and-morty-api-react/static/js/main.9793da1b.chunk.js"
+  },
+  {
+    "revision": "71b257fd4cbef08a332d",
+    "url": "/rick-and-morty-api-react/static/js/runtime-main.09de0f80.js"
+  }
+]);
